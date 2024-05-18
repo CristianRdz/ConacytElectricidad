8f2c34fbdbf94c40688d5f3dce113f8224f2c26f
CristianRdz <20213tn086@utez.edu.mx>
2024-05-17 21:29:57 -0600

    Requirements .txt

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
-
-from functions import getEnergyByCords, capacidadRed
-
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-from fastapi import FastAPI, Request
 from starlette.middleware.cors import CORSMiddleware
 
 from functions import getEnergyByCords, capacidadRed
